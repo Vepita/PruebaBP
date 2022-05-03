f5145b2e65ff89aae66e4173da219da587ba1b3f
Vepita <35640194+Vepita@users.noreply.github.com>
2022-05-02 20:40:06 -0500

    scripts base datos

---
+++ scriptsBaseDatos/client_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+-- Archivo creado  - lunes-mayo-02-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence CLIENTE_ID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CUENTAS_BP"."CLIENTE_ID_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
---
+++ scriptsBaseDatos/movimiento_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+-- Archivo creado  - lunes-mayo-02-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence MOVIMIENTO_ID_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CUENTAS_BP"."MOVIMIENTO_ID_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
